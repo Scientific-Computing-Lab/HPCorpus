ffba319d19b0ced689b2b874af3aff0b28ccc30d
Tal Kadosh <talkad@post.bgu.ac.il>
2023-07-06 22:19:43 +0300

    Implementation of deduplication of forked repositories

--- generate_stats.py
+++ generate_stats.py
@@ -3,7 +3,6 @@ from datetime import datetime
 import json
 
 
-
 def unite_paradigms():
     '''
     Join the different JSON files by repo name
@@ -16,18 +15,16 @@ def unite_paradigms():
 
             for repo, pars in paradigms.items():
                 if repo not in repos:
-
                     repos[repo] = {'CUDA': False, 'OpenCL': False, 'OpenACC': False, 'SYCL': False, 
                                           'TBB': False, 'Cilk': False, 'OpenMP': False, 'MPI': False}
 
                 for par, val in pars.items():
-                    if val:
-                        repos[repo][par] = True
+                    repos[repo][par] |= val
 
-        with open('total_paradigms.json'.format(dir), 'w') as f:
-            f.write(json.dumps(repos))
+    with open('analyzed_data/total_paradigms.json', 'w') as f:
+        f.write(json.dumps(repos))
 
-
+    
 def get_repo_metadata(metadata_filepath):
     '''
     Load metadata - creation and last update time of each repo
@@ -42,13 +39,14 @@ def get_repo_metadata(metadata_filepath):
         for line in list(dict_reader):
             creation_time = datetime.strptime(line['creation_time'], '%Y-%m-%dT%H:%M:%SZ')
             update_time = datetime.strptime(line['update_time'], '%Y-%m-%dT%H:%M:%SZ')
+            fork = True if line['fork'] == 'True' else False
 
-            metadata[line['URL'][len(prefix): -len(suffix)]] = {'creation_time': creation_time, 'update_time': update_time}
+            metadata[line['URL'][len(prefix): -len(suffix)]] = {'creation_time': creation_time, 'update_time': update_time, 'fork': fork}
 
     return metadata
 
 
-def get_paradigms_over_time(paradigm_list, metadata_filepath):
+def get_paradigms_over_time(paradigm_list, metadata_filepath, avoid_fork=True, key='update_time'):
     '''
     Get the number of repositories that used different parallelization APIs per month.
     '''
@@ -61,7 +59,11 @@ def get_paradigms_over_time(paradigm_list, metadata_filepath):
 
         for repo, paradigms in paradigm_per_repo.items():
             if repo in metadata and all([val for paradigm ,val in paradigms.items() if paradigm in paradigm_list]):
-                dt_object = metadata[repo]['update_time']
+
+                if avoid_fork and metadata[repo]['fork']:
+                    continue
+
+                dt_object = metadata[repo][key]
 
                 year = dt_object.year
                 month = dt_object.month
@@ -77,18 +79,72 @@ def get_paradigms_over_time(paradigm_list, metadata_filepath):
     return repos_over_time
 
 
+def get_total_repos_over_time(metadata_filepath):
+    '''
+    Figure 2
+
+    Get the total usage of HPCorpus repositories
+    '''
+    paradigm_per_year = {}
+    paradigm_over_time = get_paradigms_over_time([], metadata_filepath, avoid_fork=False, key='update_time')
+
+    for year in paradigm_over_time:
+        paradigm_per_year[year] = sum(paradigm_over_time[year].values())
+
+    return paradigm_per_year
+
+
+def aggregate_paradigms(metadata_filepath):
+    '''
+    Figure 3
+
+    Aggregate the usage of each parallelization API.
+    '''
+    metadata = get_repo_metadata(metadata_filepath)
+    count_paradigms = {'CUDA': 0, 'OpenCL': 0, 'OpenACC': 0, 'SYCL': 0, 
+                            'TBB': 0, 'Cilk': 0, 'OpenMP': 0, 'MPI': 0}
+    amount_repos = 0
+    amount_repo_forked = 0
+    amount_missed_repo = 0
+
+    with open('analyzed_data/total_paradigms.json', 'r') as f:
+        paradigm_per_repo = json.loads(f.read())
+
+        for repo_name, paradigms in paradigm_per_repo.items():
+            
+            if repo_name not in metadata:
+                amount_missed_repo += 1
+                continue
+
+            if metadata[repo_name]['fork']:
+                amount_repo_forked += 1
+                continue
+
+            amount_repos += 1
+
+            for paradigm, val in paradigms.items():
+                if val:
+                    count_paradigms[paradigm] += 1
+
+        print(f'Amount of valid repos: {amount_repos}')
+        print(f'Amount of repos not exist in metadata: {amount_missed_repo}')
+        print(f'Amount of forked repos: {amount_repo_forked}')
+
+        return count_paradigms
+    
+
 def cumulative_openmp(metadata_filepath):
     '''
-    Figure 5
+    Figure 4
 
     Accumulate the usage of OpenMP API over the last decade
     '''
     total = 0
     openmp_over_time_cumulative = {}
 
-    openmp_over_time = get_paradigms_over_time(['OpenMP'], metadata_filepath)
+    openmp_over_time = get_paradigms_over_time(['OpenMP'], metadata_filepath, avoid_fork=True, key='update_time')
 
-    for year in range(2012, 2024):
+    for year in range(2008, 2024):
         for month in range(12):
             y, m = year, month+1
 
@@ -103,14 +159,12 @@ def cumulative_openmp(metadata_filepath):
     return openmp_over_time_cumulative
 
 
-def get_paradigm_per_year(paradigms, metadata_filepath):
+def get_paradigm_per_year(paradigms, metadata_filepath, key='creation_time'):
     '''
-    Figure 6 + 7
-
     Get the usage of a given parallelization API over the last decade.
     '''
     paradigm_per_year = {}
-    paradigm_over_time = get_paradigms_over_time(paradigms, metadata_filepath)
+    paradigm_over_time = get_paradigms_over_time(paradigms, metadata_filepath, key=key)
 
     for year in paradigm_over_time:
         paradigm_per_year[year] = sum(paradigm_over_time[year].values())
@@ -118,9 +172,32 @@ def get_paradigm_per_year(paradigms, metadata_filepath):
     return paradigm_per_year
 
 
+def get_paradigms_per_year(metadata_filepath):
+    '''
+    Figure 5
+
+    Get the usage of each parallelization API per year
+    '''
+    paradigms_per_year = {}
+
+    for paradigm in ['CUDA', 'OpenCL', 'OpenACC', 'SYCL', 'TBB', 'Cilk', 'OpenMP', 'MPI']:
+        paradigms_per_year[paradigm] = get_paradigm_per_year([paradigm], metadata_filepath, key='update_time')
+
+    return paradigms_per_year
+
+
+def get_omp_mpi_usage(metadata_filepath):
+    '''
+    Figure 6
+
+    Get the usage of OpenMP + MPI
+    '''
+    return get_paradigm_per_year(['OpenMP', 'MPI'], metadata_filepath, key='update_time')
+
+
 def get_version_per_year(metadata_filepath):
     '''
-    Figure 9
+    Figure 7
 
     Get the usage of each OpenMP version across the last decade
     '''
@@ -153,29 +230,9 @@ def get_version_per_year(metadata_filepath):
     return versions_per_year
 
 
-def aggregate_paradigms():
-    '''
-    Figure 3 + 4
-
-    Aggregate the usage of each parallelization API.
-    '''
-    count_paradigms = {'CUDA': 0, 'OpenCL': 0, 'OpenACC': 0, 'SYCL': 0, 
-                                          'TBB': 0, 'Cilk': 0, 'OpenMP': 0, 'MPI': 0}
-
-    with open('analyzed_data/total_paradigms.json', 'r') as f:
-        paradigm_per_repo = json.loads(f.read())
-
-        for paradigms in paradigm_per_repo.values():
-            for paradigm, val in paradigms.items():
-                if val:
-                    count_paradigms[paradigm] += val
-
-        return count_paradigms
-    
-
 def aggregate_versions(lang):
     '''
-    Figure 8 + 10
+    Figure 8 - 11
 
     Aggregate the usage of each OpenMP directive
     '''
